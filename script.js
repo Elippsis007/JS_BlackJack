c7bd9ec8af50a578bf2b16c8ddf20f7e0286f292
Elippsis007 <michaeldrmm@gmail.com>
2023-09-03 22:29:22 +0100

    saving as finished assigning strings to button

--- script.js
+++ script.js
@@ -7,16 +7,20 @@ let playerAlive = true
 // this was created to remove the console.log 
 let message = ""
 
-// if sum (of two cards) is less than 21 then return message
-if (sum < 21) {
-    message = "Do you want to draw another card?"
-// or else if sum is the same or equal to 21 return message
-} else if (sum === 21) {
-    message = "BlackJack!"
-    hasBlackJack = true
-// else the player is out of the game as they have surpassed 21 with the cards they drew
-} else message = "Bust!"
-    playerAlive = false
+let messageEl = document.getElementById('message-el')
 
-// Cash Out!
-console.log(message)
+function startGame() {
+    // if sum (of two cards) is less than 21 then return message
+    if (sum <= 20) {
+        message = "Do you want to draw another card?"
+        // or else if sum is the same or equal to 21 return message
+    } else if (sum === 21) {
+        message = "BlackJack!"
+        hasBlackJack = true
+        // else the player is out of the game as they have surpassed 21 with the cards they drew
+    } else { message = "Bust!"
+            playerAlive = false
+    }
+    messageEl.textContent = message
+}
+
