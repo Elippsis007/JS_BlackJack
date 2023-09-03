f5298d0d8021749624be49787edf03b1654fa70a
Elippsis007 <michaeldrmm@gmail.com>
2023-09-03 21:30:04 +0100

    worked on logic, set up css and index.html

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstCard = 6
+let secondCard = 7
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let playerAlive = true
+// created variable called message and assigned it an empty string
+// this was created to remove the console.log 
+let message = ""
+
+// if sum (of two cards) is less than 21 then return message
+if (sum < 21) {
+    message = "Do you want to draw another card?"
+// or else if sum is the same or equal to 21 return message
+} else if (sum === 21) {
+    message = "BlackJack!"
+    hasBlackJack = true
+// else the player is out of the game as they have surpassed 21 with the cards they drew
+} else message = "Bust!"
+    playerAlive = false
+
+// Cash Out!
+console.log(message)
